6cd7346c63ce1439ef5d31d56865b810b55d334a
xuhongyu <1634501072@qq.com>
2024-10-21 17:32:35 +0800

    update: webpack 代码分离

--- 25webpack/src/index.js
+++ 25webpack/src/index.js
@@ -1,9 +1,27 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './styles.css';
 import myIcon from './project-icon.png';
+import toml from './data.toml';
 
-function component() {
+console.log(toml.title);
+
+
+// function component() {
+//   const element = document.createElement('div');
+//   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+//   element.classList.add('text');
+//   const icon = new Image();
+//   icon.src = myIcon;
+//   element.appendChild(icon);
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+async function getComponent() {
   const element = document.createElement('div');
+  // 动态引入lodash, lodash 会分离到一个单独的 bundle
+  const { default: _ } = await import('lodash');
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
   element.classList.add('text');
   const icon = new Image();
@@ -12,4 +30,6 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((comp) => {
+  document.body.appendChild(comp);
+})
--- 25webpack/webpack.config.js
+++ 25webpack/webpack.config.js
@@ -1,11 +1,44 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    // another: './src/another-module.js',
+    // 在配置文件中配置 dependOn 选项，以在多个 chunk 之间共享模块
+    // index: {
+    //   import: './src/index.js',
+    //   dependOn: 'shared'
+    // },
+    // another: {
+    //   import: './src/another-module.js',
+    //   dependOn: 'shared',
+    // },
+    // shared: 'lodash',
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // 构建前清理dist文件夹
+  },
+  devServer: {
+    static: './dist', //  将 dist 目录下的文件作为可访问资源部署在 localhost:8080
+  },
+  // 想要在一个 HTML 页面上使用多个入口起点，还需设置 optimization.runtimeChunk: 'single'
+  // optimization: {
+  //   // runtimeChunk: 'single',
+  //   // 公共依赖，多入口均单独打包
+  //   splitChunks: {
+  //     chunks: 'all',
+  //   }
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({})
+  ],
   module: {
     rules: [
       // 处理css文件
@@ -17,6 +50,41 @@ module.exports = {
       {
         test: /\.(png|jpg|jpeg|svg|gif)$/i,
         type: 'asset/resource',
+      },
+      // 处理字体文件
+      {
+        test: /\.(woff|woff2|eto|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      // 自定义处理json模块（将任何 toml、yaml 或 json5 文件作为 JSON 模块导入）
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        }
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        }
       }
     ]
   }
